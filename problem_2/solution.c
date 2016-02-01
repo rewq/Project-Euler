0af3f55c563510948a27dc0ec498a74536d8e176
Dylanmm <dmurphymancini@gmail.com>
2016-01-31 22:45:30 -0500

    update

---
+++ problem_2/solution.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+#define UPPER_LIMIT 4000000
+
+int main(void) {
+
+    int x = 1, y = 1, z = 1, s = 0;
+    while (z <= UPPER_LIMIT) {
+        if (z % 2 == 0){
+            s += z;
+        }
+        z = x + y;
+        x = y;
+        y = z;
+    }
+ 
+    printf("= %d\n", s);
+ 
+    return 0;
+}
